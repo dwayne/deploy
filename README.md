c88ed2ebc4a4df59dfcb47e8a03b6bb48a331d87
Dwayne Crooks <me@dwaynecrooks.com>
2026-02-01 08:22:13 -0400

    Add an example and start to write a README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Deploy Static Web Sites
+
+A Nix flake that provides a Bash script that's useful for deploying static websites.
---
+++ example/flake.nix
@@ -0,0 +1,55 @@
+{
+  inputs.deploy = {
+    url = ../.;
+    inputs.nixpkgs.follows = "nixpkgs";
+    inputs.flake-utils.follows = "flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, deploy }:
+    flake-utils.lib.eachDefaultSystem(system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        example = pkgs.runCommand "example" {} ''
+          mkdir "$out"
+          cp ${./index.html} "$out/index.html"
+        '';
+
+        serveExample = pkgs.writeShellScript "serve-example" ''
+          ${pkgs.caddy}/bin/caddy file-server --browse \
+            --listen :8000 \
+            --root ${example}
+        '';
+
+        deployExample = pkgs.writeShellScript "deploy-example" ''
+          ${deploy.packages.${system}.default}/bin/deploy "$@" ${example} gh-pages
+        '';
+
+        mkApp = { drv, description }: {
+          type = "app";
+          program = "${drv}";
+          meta.description = description;
+        };
+      in
+      {
+        packages = {
+          inherit example;
+          default = example;
+        };
+
+        apps = {
+          default = self.apps.${system}.example;
+
+          example = mkApp {
+            drv = serveExample;
+            description = "The example website";
+          };
+
+          deploy = mkApp {
+            drv = deployExample;
+            description = "Deploy the example website";
+          };
+        };
+      }
+    );
+}
